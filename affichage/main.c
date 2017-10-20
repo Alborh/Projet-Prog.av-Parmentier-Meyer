a5cf8dde4d5bd7ea48d8eedcec1f67de97b0f123
Alborh <albin2.parmentier@gmail.com>
2017-10-20 11:44:33 +0200

    Affiche le terrain
    Doit créer sprites manquants

---
+++ affichage/main.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 1024
+#define MAP_SIZE 32
+#define SPRITE_TERRAIN_SIZE 32
+
+int main()
+{
+  SDL_Surface *screen, *temp, *vide;
+  int gameover;
+  //lecture du fichier terrain
+  int i, j =0;
+  char **tableTerrain = (char**)malloc(MAP_SIZE*sizeof(char*));
+  for(i = 0; i<MAP_SIZE; i++)
+    {
+      tableTerrain[i] = (char*)malloc(MAP_SIZE*sizeof(char));
+    }  
+  FILE *file = NULL; 
+  file = fopen("Terrain.txt","r");
+  char c='a';
+  i = 0;
+  while((c != EOF) && (j<32))
+    {
+      c = fgetc(file);      
+      if( c != '\n')
+	{
+	  tableTerrain[i][j] = c;
+	  i++;
+	}
+      if (i > 31)
+	{
+	  i = 0;
+	  j++;
+	}      
+    }
+  fclose(file);
+  i = 0;
+  j = 0;
+  while(j<32)
+    {
+      while(i<32)
+	{
+	  printf("%c ",tableTerrain[i][j]);
+	  i++;
+	}
+      printf("\n");
+      i = 0;
+      j++;
+    }
+  //affichage de la fenetre
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_WM_SetCaption("Projet","Projet");
+  screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0);
+  //chargement des sprites
+  temp = SDL_LoadBMP("Vide.bmp");
+  vide = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  SDL_Surface *grass;
+  temp = SDL_LoadBMP("grass.bmp");
+  grass = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  SDL_Surface *arbre;
+  temp = SDL_LoadBMP("Arbre.bmp");
+  arbre = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  SDL_Surface *road;
+  temp = SDL_LoadBMP("road.dib.bmp");
+  road = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  SDL_Surface *dalle;
+  temp = SDL_LoadBMP("Dalle.bmp");
+  dalle = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  SDL_Surface *coffre;
+  temp = SDL_LoadBMP("Coffre.bmp");
+  coffre = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  SDL_Surface *eau;
+  temp = SDL_LoadBMP("Eau.bmp");
+  eau = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  SDL_Surface *bridge_v;
+  temp = SDL_LoadBMP("Bridge_V.bmp");
+  bridge_v = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  
+  gameover = 0;
+
+  while(!gameover)
+    {
+      SDL_Event event;
+      //Uint8 *keystate;
+      //événement pour quitter la fenetre
+      if (SDL_PollEvent(&event)) 
+	{          
+	  switch (event.type) 
+	    {
+	    case SDL_QUIT:
+	      gameover = 1;
+	      break;               
+	    case SDL_KEYDOWN:
+	      switch (event.key.keysym.sym) {
+	      case SDLK_ESCAPE:
+	      case SDLK_q:
+		gameover = 1;
+		break;
+	      default:
+		break;
+	      }
+	      break;
+            }
+        }
+      for(int x = 0; x<MAP_SIZE; x++)
+	{
+	  for(int y = 0; y<MAP_SIZE; y++)
+	    {
+	      SDL_Rect position;
+	      position.x = x * SPRITE_TERRAIN_SIZE;
+	      position.y = y * SPRITE_TERRAIN_SIZE;
+	      SDL_BlitSurface(vide, NULL, screen, &position);
+	      if(tableTerrain[x][y]=='g')
+		{
+		  SDL_BlitSurface(grass, NULL, screen, &position);
+		}
+	      if(tableTerrain[x][y]=='t')
+		{
+		  SDL_BlitSurface(arbre, NULL, screen, &position);
+		}
+	      if(tableTerrain[x][y]=='r')
+		{
+		  SDL_BlitSurface(road, NULL, screen, &position);
+		}
+	      if(tableTerrain[x][y]=='s')
+		{
+		  SDL_BlitSurface(dalle, NULL, screen, &position);
+		}
+	      if(tableTerrain[x][y]=='C')
+		{
+		  SDL_BlitSurface(coffre, NULL, screen, &position);
+		}
+	      if(tableTerrain[x][y]=='w')
+		{
+		  SDL_BlitSurface(eau, NULL, screen, &position);
+		}
+	      if(tableTerrain[x][y]=='B')
+		{
+		  SDL_BlitSurface(bridge_v, NULL, screen, &position);
+		}
+	    }
+	}
+      SDL_UpdateRect(screen,0,0,0,0);
+    }
+  
+  return 0;
+}
---
+++ affichage/main.cpp
@@ -0,0 +1,119 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <SDL/SDL.h>
+
+void Lecture_tableau (char tableau[][33], FILE *fichier );
+void pause();
+
+
+int main(int argc, char *argv[])
+{
+
+    char PeuImporte[24][33];
+
+    FILE *fichier = NULL, *fichier_bis =NULL;
+    fichier = fopen("C:/Users/MEYER/Desktop/L2-informatique/Projet RPG/terrain.txt","r");
+    fichier_bis = fopen("banane","w");
+
+    Lecture_tableau(PeuImporte, fichier);
+
+
+
+    SDL_Surface *Cle_Terrain[26]={NULL};
+    Cle_Terrain[22]={SDL_LoadBMP("image/Eau.dib.bmp")};
+    Cle_Terrain[0]={SDL_LoadBMP("image/Arbre.dib.bmp")};
+    Cle_Terrain[6]={SDL_LoadBMP("image/grass.dib.bmp")};
+    Cle_Terrain[17]={SDL_LoadBMP("image/road.dib.bmp")};
+    Cle_Terrain[2]={SDL_LoadBMP("image/Coffre.dib.bmp")};
+    Cle_Terrain[18]={SDL_LoadBMP("image/Dalle.dib.bmp")};
+
+
+    SDL_Surface *ecran = NULL, *imageDeFond = NULL, *Personnage = NULL;
+
+    SDL_Rect positionFond, positionPerso;
+
+    positionFond.x = 0;
+    positionFond.y = 0;
+    positionPerso.x = 0;
+    positionPerso.y = 0;
+
+
+    SDL_Init(SDL_INIT_VIDEO); // Démarrage de la SDL (ici : chargement du système vidéo)
+
+    SDL_WM_SetIcon(SDL_LoadBMP("image/sdl_icone.bmp"),NULL);
+
+    ecran = SDL_SetVideoMode(1024, 768, 32, SDL_HWSURFACE);
+
+    SDL_WM_SetCaption("Chargement d'images en SDL", NULL);
+
+
+    imageDeFond = SDL_LoadBMP("image/grass.dib.bmp");
+    Personnage = SDL_LoadBMP("image/Hero.bmp");
+    SDL_SetColorKey(Personnage, SDL_SRCCOLORKEY,SDL_MapRGB(Personnage->format,120,195,128));
+
+
+    for (int i=0; i<24; i++)
+    {
+        for (int j=0; j<32; j++)
+        {
+            positionFond.x= j*imageDeFond->w;
+            positionFond.y= i*imageDeFond->h;
+            SDL_BlitSurface(Cle_Terrain[PeuImporte[i][j]-65], NULL, ecran, &positionFond);
+
+        }
+    }
+
+    SDL_BlitSurface(Personnage, NULL, ecran, &positionPerso);
+
+
+
+    SDL_Flip(ecran);
+    pause();
+
+    SDL_FreeSurface(imageDeFond);
+    SDL_Quit(); // Arrêt de la SDL (libération de la mémoire).
+
+    return EXIT_SUCCESS;
+}
+
+
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+        case SDL_QUIT :
+            continuer = 0;
+        case SDL_KEYDOWN:
+            switch(event.key.keysym.sym)
+            {
+            case SDLK_ESCAPE:
+                continuer = 0;
+                break;
+            }
+            break;
+        }
+    }
+
+}
+
+void Lecture_tableau (char tableau[][33], FILE *fichier )
+{
+    int i,j;
+    for (i=0; i< 24; i++)
+    {
+        for (j=0; j< 33; j++)
+        {
+            tableau[i][j] = fgetc(fichier);
+            printf("%c",tableau[i][j]);
+
+        }
+    }
+}
+
+
